31ea9c7d8396881a53a3e55fa891cb8736ef834c
Charles Hall <chall8001@gmail.com>
2021-05-27 21:26:55 -0600

    created react app

---
+++ my-app/src/test.js
@@ -0,0 +1,15 @@
+
+function Test() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+            Hello hello hello
+          </p>
+        </header>
+      </div>
+    );
+  }
+  
+  export default Test;
